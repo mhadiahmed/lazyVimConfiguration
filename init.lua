6df0ab4b1f257251c5ae09c7dea03f12a5de1808
Mhadi Ahmed <mhadiahmed63@gmail.com>
2024-09-23 17:49:59 +0300

    adding tmux vim navigator configuration

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Navigate vim panes better
+-- vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+-- vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+-- vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+-- vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
